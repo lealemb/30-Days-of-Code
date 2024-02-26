1bbfbe1bd8eb480253eff9cfb742b20442fe7bc6
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-02-26 10:50:31 +0300

    solution.cpp

---
+++ 0 - Hello, World/solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string inputString; // declare the variable to hold the input string
+    getline(cin, inputString);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    cout<<"Hello, World."<<endl;  
+    cout << inputString << endl;
+    return 0;
+}
