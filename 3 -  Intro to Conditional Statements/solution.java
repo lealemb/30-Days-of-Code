9cc39af0975a5a19fdebe557b946acb804e02da6
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-02-29 22:58:07 +0300

    easy java solution

---
+++ 3 -  Intro to Conditional Statements/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+          if (N % 2 == 1 || (N % 2 == 0 && N >= 6 && N <= 20)) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+        bufferedReader.close();
+    }
+}
