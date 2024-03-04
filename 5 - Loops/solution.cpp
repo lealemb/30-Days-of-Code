d0559c702876db29032ac198dbcef1f048183a98
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-03-05 01:59:42 +0300

    Update solution.cpp

--- 5 - Loops/solution.cpp
+++ 5 - Loops/solution.cpp
@@ -1,7 +1,14 @@
-if(2<=n<=20)
-     {
-        for (int i = 1; i <= 10; i++) 
-        {
-            cout <<n<<" "<<"x"<<" "<<i<<" "<<"="<<" "<<(n*i)<<endl;
-        }
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i < 11; i++) {
+        cout <<n<<" "<<"x"<<" "<<i<<" "<<"="<<" "<<(n*i)<<endl;
     }
+
+    return 0;
+}
