a78fd9dcd14540c195a9252682630bb91bb6f3ee
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-03-05 02:24:53 +0300

    Create solution.py

---
+++ 7 - Arrays/solution.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    for num in reversed(arr):
+        print(num, end=' ')
