1f1c5c9f5e228bfcc88ab8e3c56466f66a320666
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2024-03-08 16:40:42 +0300

    Update solution.py

--- 10 - Binary Numbers/solution.py
+++ 10 - Binary Numbers/solution.py
@@ -1,17 +1,23 @@
-num = int(input())
+#!/bin/python3
 
-result = 0
-maximum = 0
+import math
+import os
+import random
+import re
+import sys
 
-while num > 0:
-    if num % 2 == 1:
-        result += 1
-        if result > maximum:
-            maximum = result
+def max_consecutive_ones(n):
+    temp = 0
+    res = 0
+    while n != 0:
+        if n % 2 == 1:
+            temp += 1
+        else:
+            temp = 0
+        res = max(temp, res)
+        n //= 2
+    return res
 
-    else:
-        result = 0
-
-    num //= 2
-
-print(maximum)
+if __name__ == '__main__':
+    n = int(input().strip())
+    print(max_consecutive_ones(n))
